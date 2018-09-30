70765d7c010829ccf6ed174e70c84a2d2b62fec6
John Bell <2206568B@student.gla.ac.uk>
2018-09-30 15:03:14 +0100

    Added dummy AJAX code for buttons

--- Code/SPEncoder.py
+++ Code/SPEncoder.py
@@ -1,6 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def main_page():
 	return render_template("SPEncoder.html")
+
+# AJAX code for buttons, dummy code for now
+@app.route("/encode")
+def encode():
+	code = request.args.get('sepi_code', "", type=str)
+	return jsonify(encoded = "[[" + code + "]]")
+
+@app.route("/run_sepi")
+def run_sepi():
+	code = request.args.get('sepi_code', "", type=str)
+	return jsonify(result = code)
+
+@app.route("/run_lipi")
+def run_lipi():
+	code = request.args.get('lipi_code', "", type=str)
+	return jsonify(result = code)
